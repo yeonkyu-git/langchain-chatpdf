6f5b94d757e2d072d5c73ce5a02500ab77037be6
yeonkyu <yk0924.kim@gmail.com>
2025-10-18 12:35:15 +0700

    Requirmentes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings, ChatGoogleGenerativeAI
 from langchain_chroma import Chroma
-from langchain.chains import create_retrieval_chain
 from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 from langchain_core.prompts import ChatPromptTemplate
 import streamlit as st  
 import tempfile
